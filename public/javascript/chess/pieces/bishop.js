b81c153dac3b99347272450101e621c1fdd99cd5
Harsh <hk755063@gmail.com>
2024-09-26 02:49:40 +0530

    bishop implemented

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -4,8 +4,29 @@ var Bishop = function(config){
 };
 
 
-
 Bishop.prototype = new Piece({});
-Bishop.prototype.move = function(newPosition){
 
-}
+
+Bishop.prototype.isValidPosition = function(targetPosition){
+    let currentCol = this.position[0];
+    let currentRow = this.position.charCodeAt(1);
+
+    let targetCol = targetPosition[0];
+    let targetRow = targetPosition.charCodeAt[1];
+
+    if (Math.abs(currentCol.charCodeAt(0) - targetCol.charCodeAt(0)) === Math.abs(currentRow - targetRow) && (targetCol>='A' && targetCol<='H') && (targetRow>='1' && targetRow<='8')) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
+
+Bishop.prototype.move = function(newPosition){
+    if (this.isValidPosition(newPosition)) {
+        this.position = newPosition.col + newPosition.row;
+        this.render();
+    } else {
+        console.error("Invalid move for bishop");
+    }
+};
