ab8980f13e07fefd3d92460d573a9960f1944ca8
Harsh <hk755063@gmail.com>
2024-09-26 02:58:42 +0530

    minor changes to bishop

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -9,10 +9,10 @@ Bishop.prototype = new Piece({});
 
 Bishop.prototype.isValidPosition = function(targetPosition){
     let currentCol = this.position[0];
-    let currentRow = this.position.charCodeAt(1);
+    let currentRow = this.position[1];
 
     let targetCol = targetPosition[0];
-    let targetRow = targetPosition.charCodeAt[1];
+    let targetRow = targetPosition[1];
 
     if (Math.abs(currentCol.charCodeAt(0) - targetCol.charCodeAt(0)) === Math.abs(currentRow - targetRow) && (targetCol>='A' && targetCol<='H') && (targetRow>='1' && targetRow<='8')) {
         return true;
