74882dffe40debf3ba3a7a0465f20e0a21b385e7
charanbhatia <charanbhatia05@gmail.com>
2024-09-26 23:54:19 +0530

    knight implemented

--- public/javascript/chess/pieces/knight.js
+++ public/javascript/chess/pieces/knight.js
@@ -1,11 +1,58 @@
-var Knight = function(config){
+var Knight = function(config, board) {
     this.type = 'knight';
+    this.board = board; 
     this.constructor(config);
 };
 
-
-
 Knight.prototype = new Piece({});
-Knight.prototype.move = function(newPosition){
 
-}
+Knight.prototype.isValidMove = function(targetPosition) {
+    let currentCol = this.position.charCodeAt(0) - 65; 
+    let currentRow = parseInt(this.position.charAt(1)) - 1;
+    let targetCol = targetPosition.col.charCodeAt(0) - 65;
+    let targetRow = parseInt(targetPosition.row) - 1;
+
+    let colDiff = Math.abs(targetCol - currentCol);
+    let rowDiff = Math.abs(targetRow - currentRow);
+
+    if (!((colDiff === 2 && rowDiff === 1) || (colDiff === 1 && rowDiff === 2))) {
+        console.warn("Invalid move for knight: not an L-shape");
+        return false;
+    }
+
+    let pieceAtTarget = this.board.getPieceAt(targetPosition);
+    if (pieceAtTarget) {
+        if (pieceAtTarget.color === this.color) {
+            console.warn("Invalid move for knight: cannot capture own piece");
+            return false;
+        } else {
+            return 'capture'; 
+        }
+    }
+    return true;
+};
+
+Knight.prototype.move = function(newPosition) {
+    const result = this.isValidMove(newPosition);
+    if (result === true) {
+        this.position = newPosition.col + newPosition.row;
+        this.render();
+        return true;
+    } else if (result === 'capture') {
+        let pieceToCapture = this.board.getPieceAt(newPosition);
+        if (pieceToCapture) {
+            pieceToCapture.kill();
+        }
+        this.position = newPosition.col + newPosition.row;
+        this.render();
+        return true;
+    }
+    return false;
+};
+
+Knight.prototype.kill = function() {
+    if (this.$el && this.$el.parentNode) {
+        this.$el.parentNode.removeChild(this.$el);
+    }
+    this.position = null;
+};
